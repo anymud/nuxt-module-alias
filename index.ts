6b7739ab5950aea6f627e9db289f43ba5748c2c1
shtse8 <shtse8@outlook.com>
2024-02-26 01:09:51 +0800

    second commit

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+
+import { defineNuxtModule } from '@nuxt/kit'
+import moduleAlias from 'module-alias'
+import { createConsola } from 'consola'
+const consola = createConsola().withTag('alias')
+
+export default defineNuxtModule({
+  meta: {
+    name: '@anymud/nuxt-module-alias',
+  },
+  async setup(options, nuxt) {
+    consola.info('Setting up module-alias')
+    for (const [key, value] of Object.entries(nuxt.options.alias)) {
+      consola.success(`Adding alias ${key} -> ${value}`) 
+      moduleAlias.addAlias(key, value)
+    }
+  },
+})
+  
